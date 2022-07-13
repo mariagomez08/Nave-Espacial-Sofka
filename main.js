bae5f6756ff6e9188dbdb184f38bf3434a28b852
mariagomez08git config --global user.email mariiangelgomezsg@gmail.comgit config --global user.name mariagomez08 <mariiangelgomezsg@gmail.com>
2022-07-13 04:31:13 -0500

    creacion de carpetas

---
+++ main.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+(async () =>{
+    await mongoose.connect('mongodb+srv://BaseDeDatos:6626846@cluster0.sqnxf.mongodb.net/?retryWrites=true&w=majority')
+        .then(()=>{console.log('Base de datos conectada')})
+        .catch( error => {console.log('Error al conectar con la base de datos', error)});
+})();
+
