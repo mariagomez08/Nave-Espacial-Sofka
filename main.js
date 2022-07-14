b2ff7688d560518e8f447d3ee0fe078fe0913f28
mariagomez08git config --global user.email mariiangelgomezsg@gmail.comgit config --global user.name mariagomez08 <mariiangelgomezsg@gmail.com>
2022-07-14 02:09:15 -0500

    commit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
 
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded())
+
+app.use(express.static('vista'))
 
 (async () =>{
     await mongoose.connect('mongodb+srv://BaseDeDatos:6626846@cluster0.sqnxf.mongodb.net/?retryWrites=true&w=majority')
@@ -7,7 +16,17 @@ const mongoose = require('mongoose');
         .catch( error => {console.log('Error al conectar con la base de datos', error)});
 })();
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'vista/index.html'))
+})
 
+app.post('/ingresarNave', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en puesto 4000')
+})
 
 
 
