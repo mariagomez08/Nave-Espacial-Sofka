ef169ae037c43d54a43514d2afa82a414bead4f4
mariagomez08git config --global user.email mariiangelgomezsg@gmail.comgit config --global user.name mariagomez08 <mariiangelgomezsg@gmail.com>
2022-07-14 03:32:52 -0500

    commit

--- main.js
+++ main.js
@@ -4,24 +4,24 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
-
-app.use(express.urlencoded())
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('vista'))
 
-(async () =>{
-    await mongoose.connect('mongodb+srv://BaseDeDatos:6626846@cluster0.sqnxf.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://BaseDeDatos:6626846@cluster0.sqnxf.mongodb.net/?retryWrites=true&w=majority')
         .then(()=>{console.log('Base de datos conectada')})
         .catch( error => {console.log('Error al conectar con la base de datos', error)});
-})();
 
-app.get('/', (req, res) => {
+
+app.get('/', (req, res) => { //metodo buscar
     res.sendFile(path.resolve(__dirname, 'vista/index.html'))
 })
 
 app.post('/ingresarNave', (req, res) => {
     console.log(req.body)
+    res.sendFile(path.resolve(__dirname, 'vista/index.html'))
 })
 
 app.listen(4000, () => {
